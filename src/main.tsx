cfba37c1fdf705083dbcb45a680c0d1c1d4ba504
Martin-Peter Lakatos <martin-peter.lakatos@gymbeam.com>
2025-12-08 00:47:20 +0100

    Added Tailwind CSS and Prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 // Render the app
 const rootElement = document.getElementById("root")!;
@@ -9,7 +10,7 @@ if (!rootElement.innerHTML) {
 
   root.render(
     <StrictMode>
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-bold text-blue-500">Hello World</h1>
     </StrictMode>,
   );
 }
